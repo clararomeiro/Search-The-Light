d80bf4341e53fe52afb1c1440225bcbbfb46719f
Ana Clara Romeiro <57761162+clararomeiro@users.noreply.github.com>
2022-05-01 22:02:10 -0300

    grab action added

---
+++ Search-The-Light/Assets/Script/GrabObject.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabObject : MonoBehaviour
+{
+    [SerializeField]
+    private Transform grabPosition;
+    private GameObject grabObject;
+    private GameObject grabbedObject;
+    private bool canGrab = false;
+    private bool isGrabbing = false;
+    [SerializeField]
+    private Animator playerAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            if (canGrab)
+            {
+                playerAnim.SetBool("grab", true);
+                grabbedObject = grabObject;
+                grabObject = null;
+                grabbedObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                grabbedObject.GetComponent<Rigidbody2D>().simulated = false;
+                grabbedObject.transform.SetParent(this.transform);
+                canGrab = false;
+                isGrabbing = true;
+
+            }
+
+            else
+            {
+                if (isGrabbing)
+                {
+                    playerAnim.SetBool("grab", false);
+                    grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                    grabbedObject.GetComponent<Rigidbody2D>().simulated = true;
+                    grabbedObject.transform.SetParent(null);
+                    grabbedObject = null;
+                    isGrabbing=false;
+
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Object") && grabbedObject == null)
+        {
+            canGrab = true;
+            grabObject = collision.gameObject;
+
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Object") && grabObject == collision.gameObject)
+        {
+            grabObject = null;
+            canGrab = false;
+        }
+    }
+
+
+}
