468d99aefc1adc39b2687586c897a2239d13107b
Nicole <nicolemaria030@hotmail.com>
2022-05-01 22:00:07 -0300

    Update Enemy.cs

--- Search-The-Light/Assets/Script/Enemy.cs
+++ Search-The-Light/Assets/Script/Enemy.cs
@@ -1,1 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Enemy : MonoBehaviour
+{
+
+    private Rigidbody2D rig;
+    public float speed;
+
+    public Transform rightCol;
+    public Transform leftCol;
+
+    public Transform headPoint;
+
+    private bool colliding;
+
+    public LayerMask layer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = new Vector2(speed, rig.velocity.y);
+
+        colliding = Physics2D.Linecast(rightCol.position, leftCol.position, layer);
+
+        if (colliding)
+        {
+            transform.localScale = new Vector2(transform.localScale.x * -1f, transform.localScale.y);
+            speed *= -1;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            float height = collision.contacts[0].point.y - headPoint.position.y;
+            Destroy(gameObject);
+        }
+    }
+
+}
