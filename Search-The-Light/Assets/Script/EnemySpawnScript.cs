40a69f3135e32fa3a56cbe36bed971b62bf25f76
Nicole <nicolemaria030@hotmail.com>
2022-05-08 23:38:25 -0300

    Att Enemies

--- Search-The-Light/Assets/Script/EnemySpawnScript.cs
+++ Search-The-Light/Assets/Script/EnemySpawnScript.cs
@@ -4,27 +4,32 @@ using UnityEngine;
 
 public class EnemySpawnScript : MonoBehaviour
 {
+    public int count = 0;
     public GameObject [] enemies;
-    float randX;
-    Vector2 whereToSpawn;
+    public Transform [] spawnpoint; 
+
     public float spawnRate = 2f;
     float nextSpawn = 0.0f;
+
+    private bool colliding;
+    private Rigidbody2D rig;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.time > nextSpawn)
+         if(Time.time > nextSpawn)
         {
             nextSpawn = Time.time + spawnRate;
-            randX = Random.Range(-9f, 10.5f);
             int randEnemy = Random.Range(0, enemies.Length);
-            whereToSpawn = new Vector2 (randX, transform.position.y);
-            Instantiate (enemies[randEnemy], whereToSpawn, Quaternion.identity);
+            int randSpawnPoint = Random.Range(0, spawnpoint.Length);
+            Instantiate (enemies[randEnemy], spawnpoint[randSpawnPoint].position, transform.rotation);
         }
     }
+
 }
