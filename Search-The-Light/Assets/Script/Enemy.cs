51cb69a1be3b3440a8c8b57b62ec2a1531b52dfe
aldenirtelles <aldenirtelless@gmail.com>
2022-05-09 22:24:15 -0300

    enemies' ia implementation

--- Search-The-Light/Assets/Script/Enemy.cs
+++ Search-The-Light/Assets/Script/Enemy.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed;
+    public float speed = 0;
     private Transform posPlayer;
 
     private Rigidbody2D rig;
 
     private bool colliding;
+
+    public bool spot = false;
+    public Transform inicioCP;
+    public Transform fimCP;
     
 
     // Start is called before the first frame update
@@ -17,30 +21,47 @@ public class Enemy : MonoBehaviour
     {
         rig = GetComponent<Rigidbody2D>();
         posPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+       // olhandoParaEsquerda = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(posPlayer.gameObject != null)
+        /*if(posPlayer.gameObject != null)
         // Vira o inimigo
         {
             transform.position = Vector2.MoveTowards(transform.position, posPlayer.position, speed * Time.deltaTime);
-        }
+        }*/
+        Raycasting();
+        Persegue();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-
         if(collision.gameObject.tag == "Bullet")
         {
             Destroy(gameObject, 0.25f);
             /*GameController.instance.totalScore += Score;
             GameController.instance.UpdateScoreText();*/
         }
-        
-    
     }
 
+    public void Raycasting()
+    {
+        Debug.DrawLine(inicioCP.position, fimCP.position, Color.green);
+        spot = Physics2D.Linecast(inicioCP.position, fimCP.position, 1 << LayerMask.NameToLayer("Player"));
+    }
 
+    public void Persegue()
+    {
+        if(spot == true && posPlayer.gameObject != null)
+        {
+            speed = 1;
+            transform.position = Vector2.MoveTowards(transform.position, posPlayer.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            speed = 0;
+        }
+    }
 }
